33f2e4f72b1139ce9607975d3ded5693d20ad674
Ken Stephens <kens@cad2cam.com>
2022-05-07 12:00:49 -0700

    Update Lesson9HWLEDController.py
    More parameterization.

--- Lesson9HWLEDController.py
+++ Lesson9HWLEDController.py
@@ -5,7 +5,8 @@ DutyCycle = 1.0
 DutyCycleUpperLimit = 100.0/1.0
 DutyCycleLowerLimit = 0.0/1.0
 Frequency = 100
-Increment = 100.0/8.0
+Steps = 8.0
+Increment = DutyCycleUpperLimit-DutyCycleLowerLimit/Steps
 Delay = .1
 
 LEDPin = 37
